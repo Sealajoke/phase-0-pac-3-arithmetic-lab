0dce0ef2b1e76e4f68b48e2e6e57c35855e274d1
seunaliu3@gmail.com <seunaliu3@gmail.com>
2022-08-19 18:13:34 +0100

    commit tree a48e200ccc7668517a02f208235e657524ce3299

---
+++ index.js
@@ -0,0 +1,65 @@
+function add(){
+
+}
+add()
+
+function subtract(){
+
+}
+subtract()
+
+function multiply(){
+
+}
+multiply()
+
+function divide(){
+
+}
+divide()
+
+function add(a, b){
+    return a + b
+}
+add(2,3)
+
+function subtract(a,b){
+    return a - b
+}
+subtract(20, 18)
+
+function multiply(a, b){
+    return a * b
+}
+multiply(20, 5)
+
+function divide(a, b){
+    return a / b
+}
+divide(30, 10)
+
+function increment(n){
+  n++
+  return n 
+}
+increment(20)
+
+function decrement(n){
+    n--
+    return n
+}
+
+decrement(25)
+
+function makeInt(n){
+   return parseInt(n, 10)
+}
+ makeInt("20.67")
+
+ function preserveDecimal(n){
+    return parseFloat(n)
+ }
+
+ preserveDecimal("200.10")
+
+ 
